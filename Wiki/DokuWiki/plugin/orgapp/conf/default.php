47b88adbbe9b0b0662568d534d3a4c34905949de
eiroca <39483731+eiroca@users.noreply.github.com>
2019-02-24 18:13:25 +0100

    Promoted some subprojects

--- Wiki/DokuWiki/plugin/orgapp/conf/default.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conf['width']='50%';
-$conf['height']='50%';
-$conf['name']='Organigram';
-$conf['archive']='./lib/plugins/orgapp/OrgApplet.jar';
-$conf['code']='OrganigramApplet.class';
-$conf['target']='_blank';
-$conf['type']=0;
-?>
--- Wiki/DokuWiki/plugin/orgapp/conf/metadata.php
+++
@@ -1,9 +0,0 @@
-<?php
-$meta['width']=array('string');
-$meta['height']=array('string');
-$meta['name']=array('string');
-$meta['archive']=array('string');
-$meta['code']=array('string');
-$meta['target']=array('string');
-$meta['type']=array('numeric');
-?>
